c2df2297ada196c17e3c773c3afc10c71c1eb5b6
rakimdevcraig <rakimdevcraig@gmail.com>
2021-03-01 21:00:58 -0500

    added email validation function

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ function showSuccess(input, message) {
     formControl.classList.add('success')
 }
 
+//function to check if email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//function to check the length it will take in a username,minimum length, and maximum length
+function checkLength(input, minLength, maxLength) {
+    let userInput = input.value
+    let length = userInput.length
+    if (length < minLength) {
+        console.log('not enough chars')
+    } else if (length > maxLength) {
+        console.log('too many chars')
+    }
+    console.log('input: ', length)
+}
+
 
 
 //Event Listeners
@@ -28,25 +46,29 @@ form.addEventListener('submit', function (e) {
     e.preventDefault()
     if (username.value === '') {
         showError(username, 'Please enter a Username')
-        console.log('error')
     } else {
         showSuccess(username, 'Thanks!')
-        console.log('success')
-    } if (email.value === '') {
+        checkLength(username, 3, 8)
+    }
+
+    if (email.value === '') {
         showError(email, 'Please enter an email')
+    } else if (!isValidEmail(email)) {
+        showError(email, 'Email is not valid')
     } else {
         showSuccess(email, 'Thanks!')
-        console.log('success')
-    } if (password.value === '') {
+    }
+
+    if (password.value === '') {
         showError(password, 'Please enter a password')
     } else {
         showSuccess(password, 'Thanks!')
-        console.log('success')
-    } if (password2.value === '') {
+    }
+
+    if (password2.value === '') {
         showError(password2, 'Please enter a password2')
     } else {
         showSuccess(password2, 'Thanks!')
-        console.log('success')
     }
 })
 
